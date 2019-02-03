a15a963fad336148c58d4dd1a2bea2bf9bbccb34
snkverma4401 <sandeepkumarverma4401@gmail.com>
2019-02-03 11:38:00 +0530

    good work

---
+++ SecondLevel_Cache_Example/src/str/ForOurLogic.java
@@ -0,0 +1,60 @@
+package str;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ForOurLogic { 
+
+	public static void main(String[] args)
+	{
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml"); 
+		
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session session1 = factory.openSession();
+		Transaction tx = session1.beginTransaction();
+		Object o=session1.load(Product.class,new Integer(105));
+		
+		Product s=(Product)o;
+		System.out.println("Loaded object product name is___"+s.getProName());		
+		System.out.println("Object Loaded successfully.....!!");	
+		session1.close();
+		
+		System.out.println("------------------------------");
+		System.out.println("Waiting......");
+		
+		try{ 
+			Thread.sleep(6000);
+		}
+		catch (Exception e) {
+		}		
+		
+		System.out.println("6 seconds compelted......!!!!!!!!");
+		
+		Session session2 = factory.openSession();
+		
+		Object o2=session2.load(Product.class,new Integer(105));
+		
+		Product s2=(Product)o2;
+		System.out.println("Loaded object product name is___"+s2.getProName());		
+		System.out.println("Object loaded from the database...!!");	
+		session2.close();
+		
+		
+		Session session3 = factory.openSession();		
+		Object o3=session3.load(Product.class,new Integer(105));
+		
+		Product s3=(Product)o3;
+		System.out.println("Loaded object product name is___"+s3.getProName());		
+		System.out.println("Object loaded from global cache successfully.....!!");	
+		session3.close();
+		
+		tx.commit();
+		factory.close();
+	}
+	
+}
